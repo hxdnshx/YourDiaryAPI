e1125c05384e69376cea8ad90034395085ccb4e3
Infko <r@g.gg>
2017-07-27 18:26:23 +0800

    开始填坑！

---
+++ YourDiaryAPI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedXuCSharpClass;
+
+namespace StalkerProject
+{
+    /*
+     * "User-Agent", "OhApp/" + u.
+                    default.version + " Platform/" + u.
+                    default.platform
+                        var a = {
+        domain: "https://ohshenghuo.com",
+        platform: "iOS",
+        version: 1.4
+    };
+     */
+    public class YourDiaryAPI
+    {
+        private HttpHelper inst;
+
+        public YourDiaryAPI()
+        {
+            
+        }
+    }
+}
